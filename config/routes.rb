4917a47578a09fd735658da7b7574ee56e84888f
Pradnyesh-vf64 <psharad@verticalfox.com>
2022-02-01 23:05:27 +0530

    Fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  devise_scope :users do
+  devise_scope :user do
     get 'login', to: 'devise/sessions#new'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
